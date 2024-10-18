8b2f9f9be947fe7060121f0a8b62e00fc8a60a48
Kent Ng <kent.ng201@gmail.com>
2024-10-19 07:46:57 +0800

    format js

--- src/models/Dummy.p.ts
+++ src/models/Dummy.p.ts
@@ -2,18 +2,19 @@ import { Model } from "pocketto";
 import moment from 'moment';
 
 export class Dummy extends Model {
-    static collectionName = 'Dummies';
+  static dbName = 'default';
+  static collectionName = 'Dummies';
 
-    name?: string;
-    color?: string;
+  name?: string;
+  color?: string;
 
-    setRandomName() {
-        this.name = moment().format('YYYY-MM-DD HH:mm:ss');
-        return this;
-    }
+  setRandomName() {
+    this.name = moment().format('YYYY-MM-DD HH:mm:ss');
+    return this;
+  }
 
-    setRandomHexColor() {
-        this.color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-        return this;
-    }
+  setRandomHexColor() {
+    this.color = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+    return this;
+  }
 }
